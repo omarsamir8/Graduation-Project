5f534e6d0fb0f927e1c976795f7a4d7be9ff045e
Armyluck111 <armyluck111@gmail.com>
2024-04-08 14:57:42 +0200

    Dola

--- src/ComponentForStudents/RegisterForTraining.jsx
+++ src/ComponentForStudents/RegisterForTraining.jsx
@@ -1,66 +1,91 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Swal from 'sweetalert2';
 
 export default function RegisterForTraining() {
+  const [alltrainingsAvailable,setalltrainingsAvailable]=useState([])
+  const accessToken = localStorage.getItem("accesstoken");
+  const refreshToken = localStorage.getItem("refreshtoken");
+     // Register for training
+    const RegisterForTraining = async (tainingId) => {
+      try {
+        const response = await fetch(
+          `https://university-mohamed.vercel.app//Api/Register/Training/addTraining?trainingId=${tainingId}`,
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+              "refresh-token": refreshToken,
+            },
+          }
+        );
+ 
+        if (response.ok) {
+          Swal.fire({
+            icon: "success",
+            title: "Course registered successfully",
+            showConfirmButton: false,
+            timer: 3500,
+          });
+
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Failed",
+            text: "Course register failed, please try again later",
+            timer: 4500,
+          });
+        }
+      } catch (error) {
+        console.error("Register Failed", error);
+      }
+    };
+      // Fetch all Trainings
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://university-mohamed.vercel.app/Api/training/alltraining?page=1&size=20`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+              "refresh-token": refreshToken,
+            },
+          }
+        );
+        const data = await response.json();
+        setalltrainingsAvailable(data.training);
+        console.log(data)
+      } catch (error) {
+        console.error("Fetch failed", error);
+      }
+    };
+
+    fetchData();
+  }, [accessToken, refreshToken]);
   return (
     <>
     <div className="Create_Student">
       <h2>All Trainings Available</h2>
     </div>
     <div className="enrollcourse">
-      <div className="course">
+      {alltrainingsAvailable.map((training)=>{
+        return    <div className="course">
         <div className="info">
-          <p>Object Oriented programming</p>
-          <button type="button" class="btn btn-primary">
+          <p>{training.training_name}</p>
+          <button type="button" class="btn btn-primary" onClick={()=>RegisterForTraining(training._id)}>
             Register
           </button>
         </div>
         <div className="img "></div>
       </div>
-      <div className="course">
-        <div className="info">
-          <p>Basics Of Lang Programming</p>
-          <button type="button" class="btn btn-primary">
-          Register
-          </button>
-        </div>
-        <div className="img img2"></div>
-      </div>
-      <div className="course">
-        <div className="info">
-          <p>Algorithms & Data Structure</p>
-          <button type="button" class="btn btn-primary">
-          Register
-          </button>
-        </div>
-        <div className="img img3"></div>
-      </div>
-      <div className="course">
-        <div className="info">
-          <p>Opreating System</p>
-          <button type="button" class="btn btn-primary">
-          Register
-          </button>
-        </div>
-        <div className="img img4"></div>
-      </div>
-      <div className="course">
-        <div className="info">
-          <p>E-Commerce & E-bussniss</p>
-          <button type="button" class="btn btn-primary">
-          Register
-          </button>
-        </div>
-        <div className="img img5"></div>
-      </div>
-      <div className="course">
-        <div className="info">
-          <p>Data-----Mining</p>
-          <button type="button" class="btn btn-primary">
-          Register
-          </button>
-        </div>
-        <div className="img img6"></div>
-      </div>
+      })}
+   
+    
+  
+     
+     
+    
     </div>
   </>
   )
--- src/ComponentForStudents/RegisteredTraining.jsx
+++ src/ComponentForStudents/RegisteredTraining.jsx
@@ -1,6 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function RegisteredTraining() {
+  const accessToken = localStorage.getItem("accesstoken");
+  const refreshToken = localStorage.getItem("refreshtoken");
+const [trainingsregisterd,settrainingsregisterd]=useState([])
+   // get registerd courses
+   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://university-mohamed.vercel.app/Api/Register/Training/getTrainingInfo`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+              "refresh-token": refreshToken,
+            },
+          }
+        );
+        const data = await response.json();
+        console.log(data);
+        settrainingsregisterd(data.result);
+       
+      } catch (error) {
+        console.error("Fetch failed", error);
+      }
+    };
+
+    fetchData();
+  }, [accessToken, refreshToken]);
+  console.log(trainingsregisterd);
   return (
     <>
     <div className="Create_Student">
