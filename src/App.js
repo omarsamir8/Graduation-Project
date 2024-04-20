4b85d4b44a7d611726454606a24407ffd850b9f9
omar <omarmokdad2022@gmail.com>
2024-04-20 04:53:35 +0200

    omar

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import MainPageForStudent from './pages/MainPageForStudent'
-import MainPageForDoctor from './pages/MainPageForDocTor'
-import MainPageForAdmin from './pages/MainPageForAdmin'
-import Login from './pages/Login'
-import Registered_Courses from './pages/Registered_Courses/Registered_Courses'
-import Reports_student from './pages/Reports_student/Reports_student'
-import Semester_grade from './pages/Semester_grade'
-import { routes } from './routes'
-import MainPageForSuberAdmin from './pages/MainPageForSuberAdmin'
-import Training_Info from './pages/Training_Info/Training_Info'
-import ScrollToTopButton from './ScroollButton/ScrollToTopButton '
-function App () {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPageForStudent from "./pages/MainPageForStudent";
+import MainPageForDoctor from "./pages/MainPageForDocTor";
+import MainPageForAdmin from "./pages/MainPageForAdmin";
+import Login from "./pages/Login";
+import Registered_Courses from "./pages/Registered_Courses/Registered_Courses";
+import Reports_student from "./pages/Reports_student/Reports_student";
+import Semester_grade from "./pages/Semester_grade";
+import { routes } from "./routes";
+import MainPageForSuberAdmin from "./pages/MainPageForSuberAdmin";
+import Training_Info from "./pages/Training_Info/Training_Info";
+import ScrollToTopButton from "./ScroollButton/ScrollToTopButton ";
+function App() {
   return (
     <>
-      <div className='App'>
+      <div className="App">
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<Login />} />
-            <Route path='/student' element={<MainPageForStudent />} />
-            <Route path='/doctor' element={<MainPageForDoctor />} />
-            <Route path='/admin' element={<MainPageForAdmin />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/student" element={<MainPageForStudent />} />
+            <Route path="/doctor" element={<MainPageForDoctor />} />
+            <Route path="/admin" element={<MainPageForAdmin />} />
+            <Route path="/superadmin" element={<MainPageForSuberAdmin />} />
             <Route
-              path='/superadmin'
-              element={<MainPageForSuberAdmin />}
-            />
-            <Route
-              path='/Registered_Courses'
+              path="/Registered_Courses"
               element={<Registered_Courses />}
             />
-            <Route
-              path='/Reports_student'
-              element={<Reports_student />}
-            />
-            <Route path='/trainingInfo' element={<Training_Info />} />
-            <Route path='/Semester_grade' element={<Semester_grade />} />
+            <Route path="/Reports_student" element={<Reports_student />} />
+            <Route path="/training/:trainingId" element={<Training_Info />} />
+            <Route path="/Semester_grade" element={<Semester_grade />} />
           </Routes>
         </BrowserRouter>
         <ScrollToTopButton />
       </div>
     </>
-  )
+  );
 }
-export default App
+export default App;
--- src/pages/Training_Info/Training_Info.jsx
+++ src/pages/Training_Info/Training_Info.jsx
@@ -1,52 +1,133 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, Navigate, useParams } from "react-router-dom";
 import { routes } from "../../routes";
+import "./traininginfo.css";
 
-const TrainingDetails = () => {
+const TrainingInfo = () => {
   const { trainingId } = useParams(); // Get training ID from URL parameter
-  const [trainingData, settrainingData] = useState({}); // Initialize trainingData state with null
+  const [trainingData, setTrainingData] = useState([]);
+  const [trainingimages, settrainingimages] = useState([]);
+  const [AllowLevel, setAllowLevel] = useState([]); // Initialize trainingData state with an empty object
   const accessToken = localStorage.getItem("accesstoken");
   const refreshToken = localStorage.getItem("refreshtoken");
 
   // Fetch training Info
   useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await fetch(
-            `https://university-mohamed.vercel.app${routes.Training._id}${routes.Training.singleTraininginfoBystudent}?trainingId=${trainingId}`,
-            {
-              method: "GET",
-              headers: {
-                Authorization: `Bearer ${accessToken}`,
-                "refresh-token": refreshToken,
-              },
-            }
-          );
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://university-mohamed.vercel.app${routes.Training._id}${routes.Training.singleTraininginfoBystudent}?trainingId=${trainingId}`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+              "refresh-token": refreshToken,
+            },
+          }
+        );
 
-          const data = await response.json();
-          console.log(data);
-          settrainingData(data);
-        } catch (error) {
-          console.error("Fetch failed", error);
-        }
-      };
+        const data = await response.json();
+        console.log(data);
+        setTrainingData(data.training);
+        setAllowLevel(data.training.AllowLevel);
+        settrainingimages(data.training.images);
+      } catch (error) {
+        console.error("Fetch failed", error);
+      }
+    };
 
-      fetchData();
-    },
-    [trainingId,accessToken, refreshToken]
-  );
+    fetchData();
+  }, [trainingId, accessToken, refreshToken]);
+  console.log(trainingData);
+  console.log(AllowLevel);
+  console.log(trainingimages);
 
   return (
-    <div className="enrollcourse">
-      <div className="course">
-        <div className="info">
-          <h1>Hello</h1>
-      <p>{trainingData.training.training_name}</p>
+    <>
+      <h3
+        style={{
+          textAlign: "center",
+          marginTop: "10px",
+          fontWeight: "bold",
+          fontFamily: "cursive",
+        }}
+      >
+        Training Details
+      </h3>
+      <div
+        style={{
+          margin: "auto",
+          height: "2px",
+          width: "120px",
+          backgroundColor: "black",
+        }}
+      ></div>
+      <div style={{ marginTop: "20px" }} className="productdisplay">
+        <div className="productdisplay-left">
+          <div className="productdisplay-img-list">
+            {trainingimages.map((img, index) => {
+              if (trainingimages[index] && trainingimages[index].url) {
+                return (
+                  <img key={index} src={trainingimages[index].url} alt="" />
+                );
+              } else {
+                return null;
+              }
+            })}
+
+            {trainingimages.length > 1 && (
+              <img src={trainingimages[1].url} alt="" />
+            )}
+          </div>
+          <div className="productdisplay-img">
+            {trainingimages.length > 1 && (
+              <img
+                style={{ height: "460px" }}
+                className="productdisplay-main-img"
+                src={trainingimages[0].url}
+                alt=""
+              />
+            )}
+          </div>
+        </div>
+        <div className="productdisplay-right">
+          <h1>{trainingData.training_name}</h1>
+          <div className="productdisplay-right-prices">
+            <div className="productdisplay-right-price-old"></div>
+            <div className="productdisplay-right-price-new"></div>
+          </div>
+          <div className="productdisplay-right-descriptions">
+            {trainingData.desc}
+          </div>
+          <div className="productdisplay-right-size">
+            <h1>Allow For Level</h1>
+            <div className="productdisplay-right-sizes">
+              {AllowLevel.map((level) => {
+                return <div>{level}</div>;
+              })}{" "}
+            </div>
+          </div>
+          <p
+            style={{ marginTop: "15px" }}
+            className="productdisplay-right-category"
+          >
+            <span>Open For Register :</span> {trainingData.OpenForRegister} True
+          </p>
+          <p className="productdisplay-right-category">
+            <span>Start Date :</span> {trainingData.start_date}
+          </p>
+          <p className="productdisplay-right-category">
+            <span>End Date:</span> {trainingData.end_date}
+          </p>
+          <p className="productdisplay-right-category">
+            <Link to="/student" style={{ cursor: "pointer", color: "#b893dd" }}>
+              Back~~~~
+            </Link>
+          </p>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default TrainingDetails;
-
+export default TrainingInfo;
