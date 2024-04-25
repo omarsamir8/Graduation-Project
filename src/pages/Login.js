4376f2631399dbae5f75a7bec9078287911ecfb4
omar <omarmokdad2022@gmail.com>
2024-04-25 18:57:02 +0200

    omar

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,254 +1,259 @@
-import React, { useState, useEffect } from 'react'
-import '../styles/Login.css'
-import { useNavigate } from 'react-router-dom'
-import Form from 'react-bootstrap/Form'
-import { routes } from '../routes'
-import Swal from 'sweetalert2'
+import React, { useState, useEffect } from "react";
+import "../styles/Login.css";
+import { useNavigate } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import { routes } from "../routes";
+import Swal from "sweetalert2";
 
-function Login () {
-  const navigate = useNavigate()
-  const [email, setemail] = useState('')
-  const [Student_Code, setStudent_Code] = useState('')
-  const [password, setpassword] = useState('')
-  const [message, setmessage] = useState('')
-  const [type, settype] = useState('')
-  const [name, setname] = useState('')
- 
+function Login() {
+  const navigate = useNavigate();
+  const [email, setemail] = useState("");
+  const [Student_Code, setStudent_Code] = useState("");
+  const [password, setpassword] = useState("");
+  const [message, setmessage] = useState("");
+  const [type, settype] = useState("");
+  const [name, setname] = useState("");
 
   useEffect(() => {
     // تحديث قيمة name عندما يتغير نوع المستخدم
-    if (type === 'student') {
-      setname(Student_Code)
-    } else if (type === 'doctor' || type === 'admin') {
-      setname(email)
+    if (type === "student") {
+      setname(Student_Code);
+    } else if (type === "doctor" || type === "admin") {
+      setname(email);
       // قد تحتاج إلى استخدام قيمة البريد الإلكتروني هنا
     }
-  }, [type, Student_Code, email])
+  }, [type, Student_Code, email]);
 
   const type2 = (e) => {
-    if (type === 'student') {
-      setStudent_Code(e.target.value)
-    } else if (type === 'doctor' || type === 'admin') {
-      setemail(e.target.value)
+    if (type === "student") {
+      setStudent_Code(e.target.value);
+    } else if (type === "doctor" || type === "admin") {
+      setemail(e.target.value);
     }
-  }
+  };
 
   const check = () => {
-    if (type === 'student') {
-      handleLoginForStudent()
-      console.log('this login for students')
-    } else if (type === 'doctor') {
-      handleLoginforDoctor()
-      console.log('this login for doctors')
-    } else if (type === 'admin') {
-      handleLoginforadmin()
-      console.log('this login for admins and superadmins')
+    if (type === "student") {
+      handleLoginForStudent();
+      console.log("this login for students");
+    } else if (type === "doctor") {
+      handleLoginforDoctor();
+      console.log("this login for doctors");
+    } else if (type === "admin") {
+      handleLoginforadmin();
+      console.log("this login for admins and superadmins");
     } else {
-      console.log('you must select typee ')
+      console.log("you must select typee ");
     }
-  }
+  };
 
   const handleLoginForStudent = async () => {
     try {
       const response = await fetch(
         `https://university-mohamed.vercel.app${routes.student._id}${routes.student.login}`,
         {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
           },
-          body: JSON.stringify({ Student_Code, password })
+          body: JSON.stringify({ Student_Code, password }),
         }
-      )
-      const data = await response.json()
-      console.log(data)
+      );
+      const data = await response.json();
+      console.log(data);
 
-      localStorage.setItem('accesstoken', data.accessToken)
-      localStorage.setItem('refreshtoken', data.refreshToken)
+      localStorage.setItem("accesstoken", data.accessToken);
+      localStorage.setItem("refreshtoken", data.refreshToken);
 
       if (response.ok) {
         // If login is successful, navigate to the "/student" page
-        navigate('/student')
+        navigate("/student");
       } else {
         // Show an error message if needed
         Swal.fire({
           icon: "error",
           title: "Fail",
-          text: data.error_Message[0].message,
+          text: data.error_Message
+            ? data.error_Message[0].message
+            : data.message,
           timer: 4500,
         });
       }
     } catch (error) {
-      console.error('Login failed', error)
+      console.error("Login failed", error);
     }
-  }
+  };
 
   const handleLoginforDoctor = async () => {
     try {
       const response = await fetch(
         `https://university-mohamed.vercel.app${routes.instructor._id}${routes.instructor.login}`,
         {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
           },
-          body: JSON.stringify({ email, password })
+          body: JSON.stringify({ email, password }),
         }
-      )
-      const data = await response.json()
-      console.log(data)
+      );
+      const data = await response.json();
+      console.log(data);
 
-      localStorage.setItem('accesstoken', data.accessToken)
-      localStorage.setItem('refreshtoken', data.refreshToken)
+      localStorage.setItem("accesstoken", data.accessToken);
+      localStorage.setItem("refreshtoken", data.refreshToken);
 
       if (response.ok) {
         // If login is successful, navigate to the "/doctor" page
-        navigate('/doctor')
+        navigate("/doctor");
       } else {
         // Show an error message if needed
         Swal.fire({
           icon: "error",
           title: "Fail",
-          text: data.error_Message[0].message,
+          text: data.error_Message
+            ? data.error_Message[0].message
+            : data.message,
           timer: 4500,
         });
       }
     } catch (error) {
-      console.error('Login failed', error)
+      console.error("Login failed", error);
     }
-  }
+  };
 
   const handleLoginforadmin = async () => {
     try {
       const response = await fetch(
         `https://university-mohamed.vercel.app${routes.Admin._id}${routes.Admin.login}`,
         {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
           },
-          body: JSON.stringify({ email, password })
+          body: JSON.stringify({ email, password }),
         }
-      )
-      const data = await response.json()
-      console.log(data)
+      );
+      const data = await response.json();
+      console.log(data);
 
-      localStorage.setItem('accesstoken', data.accessToken)
-      localStorage.setItem('refreshtoken', data.refreshToken)
+      localStorage.setItem("accesstoken", data.accessToken);
+      localStorage.setItem("refreshtoken", data.refreshToken);
 
-      if (response.ok && data.role === 'admin') {
+      if (response.ok && data.role === "admin") {
         // If login is successful, navigate to the "/admin" page
-        navigate('/admin')
-      } else if (response.ok && data.role === 'superAdmin') {
-        navigate('/superadmin')
+        navigate("/admin");
+      } else if (response.ok && data.role === "superAdmin") {
+        navigate("/superadmin");
       } else {
         // Show an error message if needed
         Swal.fire({
           icon: "error",
           title: "Fail",
-          text: data.error_Message[0].message,
+          text: data.error_Message
+            ? data.error_Message[0].message
+            : data.message,
           timer: 4500,
         });
       }
     } catch (error) {
-      console.error('Login failed', error)
+      console.error("Login failed", error);
     }
-  }
-  console.log(message)
+  };
+  console.log(message);
   return (
     <>
-      <div className='col-12 login-page'>
-        <div className='col-12 login-container'>
+      <div className="col-12 login-page">
+        <div className="col-12 login-container">
           {/* <form></form> */}
-          <div className=' col-12 login'>
-            <div className='col-12 title'>
-              <h1 className='col-12 login_title'>Login</h1>
-              <h1 className='col-12 login_title'>Welcome back !</h1>
+          <div className=" col-12 login">
+            <div className="col-12 title">
+              <h1 className="col-12 login_title">Login</h1>
+              <h1 className="col-12 login_title">Welcome back !</h1>
             </div>
 
             {/* <div className="inputs col-12"> */}
             {/* <h4>Login As .......</h4> */}
             {/* <p className="col-12">Enter Your Account Details</p> */}
-            <div className='kind'>
-              <label className='col-12'>Login As...</label>
-              <div className='col-12 radio_inputs'>
+            <div className="kind">
+              <label className="col-12">Login As...</label>
+              <div className="col-12 radio_inputs">
                 <input
-                  type='radio'
-                  name='kind'
-                  className='student'
-                  value='student'
+                  type="radio"
+                  name="kind"
+                  className="student"
+                  value="student"
                   onChange={(e) => {
-                    settype(e.target.value)
+                    settype(e.target.value);
                   }}
                 />
                 Student
                 <input
-                  type='radio'
-                  name='kind'
-                  value='doctor'
+                  type="radio"
+                  name="kind"
+                  value="doctor"
                   onChange={(e) => {
-                    settype(e.target.value)
+                    settype(e.target.value);
                   }}
                 />
                 Doctor
                 <input
-                  type='radio'
-                  name='kind'
-                  value='admin'
+                  type="radio"
+                  name="kind"
+                  value="admin"
                   onChange={(e) => {
-                    settype(e.target.value)
+                    settype(e.target.value);
                   }}
                 />
                 Admin
               </div>
             </div>
-            <div className='col-12 labelWithInput'>
-              <Form.Label className='input_label'>Email Or Code *</Form.Label>
+            <div className="col-12 labelWithInput">
+              <Form.Label className="input_label">Email Or Code *</Form.Label>
               <Form.Control
-                type='text'
+                type="text"
                 name={name}
                 value={name}
                 onChange={type2}
                 required
-                placeholder='Enter email or code here'
-                className='login_input'
+                placeholder="Enter email or code here"
+                className="login_input"
               />
             </div>
-            <div className='col-12 labelWithInput'>
-              <Form.Label className='input_label'>Password *</Form.Label>
+            <div className="col-12 labelWithInput">
+              <Form.Label className="input_label">Password *</Form.Label>
               <Form.Control
-                type='password'
-                name='password'
+                type="password"
+                name="password"
                 value={password}
                 onChange={(e) => {
-                  setpassword(e.target.value)
+                  setpassword(e.target.value);
                 }}
                 required
-                placeholder='Enter your password here'
-                className='login_input'
+                placeholder="Enter your password here"
+                className="login_input"
               />
             </div>
 
-            <p style={{ fontSize: '15px', color: 'red' }} className='error'>
+            <p style={{ fontSize: "15px", color: "red" }} className="error">
               {message}
             </p>
-            <a href='#forgetpassword'>Forget Password?</a>
+            <a href="#forgetpassword">Forget Password?</a>
             {/* </div> */}
-            <button className='button' onClick={check}>
+            <button className="button" onClick={check}>
               Login
             </button>
           </div>
-          <div className='logo'>
+          <div className="logo">
             <img
-              src='./assets/images/login_logo.jpg'
-              alt=''
-              className='login_logo'
+              src="./assets/images/login_logo.jpg"
+              alt=""
+              className="login_logo"
             />
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
