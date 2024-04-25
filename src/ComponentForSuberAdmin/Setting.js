228bcffdf12344b8e757d7ba0f5d037ae08220d1
Armyluck111 <armyluck111@gmail.com>
2024-04-25 16:49:24 +0200

    Dola

--- src/ComponentForSuberAdmin/Setting.js
+++ src/ComponentForSuberAdmin/Setting.js
@@ -1,23 +1,16 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-
-import { routes } from "../routes";
+import { Table, ToggleButton } from "react-bootstrap";
 import Swal from "sweetalert2";
-import { Table } from "react-bootstrap";
-import { Select } from "@mui/material";
+import { routes } from "../routes";
 
 function Setting() {
-  const [Setting, setSetting] = useState([]);
-  const [deniedRoutes, setdeniedRoutes] = useState([]);
-  const [ApiUrl, setApiUrl] = useState();
-  const [MainSemsterId, setMainSemsterId] = useState("");
-  const [MaxAllowTrainingToRegister, setMaxAllowTrainingToRegister] =
-    useState("1");
-  const [selectedSetting, setselectedSetting] = useState(null);
-  const [AllSemesters, setAllSemesters] = useState([]);
-  const [checked, setChecked] = useState(false);
+  const [settings, setSettings] = useState([]);
+  const [checkedValues, setCheckedValues] = useState({});
   const accessToken = localStorage.getItem("accesstoken");
   const refreshToken = localStorage.getItem("refreshtoken");
+
+  // Fetch setting data
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -32,7 +25,14 @@ function Setting() {
         );
 
         console.log(response.data);
-        setSetting(response.data.routedescription);
+        setSettings(response.data.routedescription);
+        
+        // Initialize checkedValues state with default values
+        const initialCheckedValues = {};
+        response.data.routedescription.forEach(setting => {
+          initialCheckedValues[setting._id] = setting.value === "yes";
+        });
+        setCheckedValues(initialCheckedValues);
       } catch (error) {
         console.error("Error fetching admin info:", error);
       }
@@ -42,10 +42,10 @@ function Setting() {
   }, [accessToken, refreshToken]);
 
   // Update Settings
-  const UpdateSettings = async () => {
+  const updateSettings = async () => {
     try {
       const response = await fetch(
-        "https://university-mohamed.vercel.app/Api/admin/setting/update",
+        `https://university-mohamed.vercel.app/Api/admin/setting/update`,
         {
           method: "PUT",
           headers: {
@@ -54,77 +54,11 @@ function Setting() {
             "refresh-token": refreshToken,
           },
           body: JSON.stringify({
-            deniedRoutes,
+            deniedRoutes: checkedValues,
           }),
         }
       );
-
-      if (response.ok) {
-        // Show SweetAlert on success
-        Swal.fire({
-          icon: "success",
-          title: "Setting updated successfully",
-          showConfirmButton: false,
-          timer: 3500,
-        });
-      } else {
-        // Show an error message if needed
-        Swal.fire({
-          icon: "error",
-          title: "Fail",
-          text: "Setting update failed, please try again later",
-          timer: 4500,
-        });
-      }
-    } catch (error) {
-      console.error("Update failed", error);
-    }
-  };
-
-  console.log(ApiUrl);
-  console.log(Setting);
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch(
-        `https://university-mohamed.vercel.app${routes.semster._id}${routes.semster.searchsemster}?page=1&size=20`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-            "refresh-token": refreshToken,
-          },
-        }
-      );
-
       const data = await response.json();
-      setAllSemesters(data.semsters);
-    } catch (error) {
-      console.error("Fetch failed", error);
-    }
-  };
-  useEffect(() => {
-    fetchData();
-  }, [accessToken, refreshToken]);
-
-  // change Main Semester
-  const ChangeMainSemester = async () => {
-    try {
-      const response = await fetch(
-        "https://university-mohamed.vercel.app/Api/admin/setting/update",
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`,
-            "refresh-token": refreshToken,
-          },
-          body: JSON.stringify({
-            MainSemsterId,
-            MaxAllowTrainingToRegister,
-          }),
-        }
-      );
 
       if (response.ok) {
         // Show SweetAlert on success
@@ -147,16 +81,20 @@ function Setting() {
       console.error("Update failed", error);
     }
   };
-  // handle checked
-  const handleCheckboxChange = (checked) => {
-    setChecked(!checked);
+
+  const handleCheckboxChange = (settingId, checked) => {
+    setCheckedValues(prevCheckedValues => ({
+      ...prevCheckedValues,
+      [settingId]: checked,
+    }));
   };
+
   return (
     <>
       <div className="setting-page">
         <div className="single-setting">
-          <Table striped bordered hover class="table" style={{}}>
-            <thead class="thead-dark">
+          <Table striped bordered hover className="table">
+            <thead className="thead-dark">
               <tr>
                 <th scope="col">#</th>
                 <th style={{ textAlign: "center" }} scope="col">
@@ -169,86 +107,40 @@ function Setting() {
               </tr>
             </thead>
             <tbody>
-              {Setting.map((setting, index) => {
-                return (
-                  <tr key={setting._id}>
-                    <th scope="row">{index + 1}</th>
-                    <td style={{ textAlign: "center" }}>{setting.name}</td>
-                    <td style={{ textAlign: "center" }}>{setting.desc}</td>
-                    <td>
-                      <div style={{}} class="form-check form-switch">
-                        <input
-                          style={{ width: "70px", height: "20px" }}
-                          class="form-check-input"
-                          type="checkbox"
-                          role="switch"
-                          id="flexSwitchCheckDefault"
-                          onChange={handleCheckboxChange} // Just pass the function reference
-                          checked={checked}
-                        />
-                      </div>
-                    </td>
-                  </tr>
-                );
-              })}
+              {settings.map((setting, index) => (
+                <tr key={setting._id}>
+                  <th scope="row">{index + 1}</th>
+                  <td style={{ textAlign: "center" }}>{setting.name}</td>
+                  <td style={{ textAlign: "center" }}>{setting.desc}</td>
+                  <td>
+                    <ToggleButton
+                      id={`flexSwitchCheck-${setting._id}`}
+                      type="checkbox"
+                      variant="outline-primary"
+                      checked={checkedValues[setting._id]}
+                      onChange={(e) =>
+                        handleCheckboxChange(setting._id, e.target.checked)
+                      }
+                    >
+                      {checkedValues[setting._id] ? "Yes" : "No"}
+                    </ToggleButton>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </Table>
-          <div
-            style={{ display: "flex", gap: "10px" }}
-            className="Change-Main-semester"
+          <button
+            style={{ height: "37px" }}
+            type="button"
+            className="btn btn-primary"
+            onClick={updateSettings}
           >
-            <select
-              class="form-select form-select-md mb-3"
-              aria-label=".form-select-lg example"
-              style={{ width: "300px" }}
-              onChange={(e) => {
-                setMainSemsterId(e.target.value);
-              }}
-            >
-              <option selected>Select Main Semester</option>
-              {AllSemesters.map((Sem) => {
-                return <option value={Sem._id}>{Sem.name}</option>;
-              })}
-            </select>
-            <select
-              class="form-select form-select-md mb-3"
-              aria-label=".form-select-lg example"
-              style={{ width: "300px" }}
-              onChange={(e) => {
-                setMaxAllowTrainingToRegister(e.target.value);
-              }}
-            >
-              <option selected>Open Number Of Training </option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
-            <button
-              style={{ height: "37px" }}
-              type="button"
-              class="btn btn-primary "
-              onClick={() => {
-                ChangeMainSemester();
-              }}
-            >
-              Change Main Semester
-            </button>
-            <button
-              style={{ height: "37px" }}
-              type="button"
-              class="btn btn-primary "
-              onClick={() => {
-                UpdateSettings();
-              }}
-            >
-              Save Changes
-            </button>
-          </div>
+            Save Changes
+          </button>
         </div>
       </div>
     </>
   );
 }
+
 export default Setting;
-
-
