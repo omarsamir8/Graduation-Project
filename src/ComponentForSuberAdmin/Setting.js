209c39813eb05538acacf7fd37c58174c5982910
Armyluck111 <armyluck111@gmail.com>
2024-04-26 03:51:12 +0300

    Dola

--- src/ComponentForSuberAdmin/Setting.js
+++ src/ComponentForSuberAdmin/Setting.js
@@ -2,19 +2,19 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { routes } from "../routes";
 import Swal from "sweetalert2";
-import { Table } from "react-bootstrap";
+import { Table, Form } from "react-bootstrap";
 
 function Setting() {
   const [Setting, setSetting] = useState([]);
-  const [deniedRoutes, setdeniedRoutes] = useState([]);
-  const [allow, setallow] = useState("yes");
+  const [ApiUrls, setApiUrls] = useState([]);
   const [MainSemsterId, setMainSemsterId] = useState("");
   const [MaxAllowTrainingToRegister, setMaxAllowTrainingToRegister] =
     useState("1");
   const [AllSemesters, setAllSemesters] = useState([]);
-  const [ApiUrls, setApiUrls] = useState([]);
+
   const accessToken = localStorage.getItem("accesstoken");
   const refreshToken = localStorage.getItem("refreshtoken");
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -37,7 +37,17 @@ function Setting() {
     fetchData();
   }, [accessToken, refreshToken]);
 
-  // Update Settings
+  useEffect(() => {
+    const storedApiUrls = JSON.parse(localStorage.getItem("ApiUrls"));
+    if (storedApiUrls) {
+      setApiUrls(storedApiUrls);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("ApiUrls", JSON.stringify(ApiUrls));
+  }, [ApiUrls]);
+
   const UpdateSettings = async () => {
     try {
       const response = await fetch(
@@ -49,12 +59,12 @@ function Setting() {
             Authorization: `Bearer ${accessToken}`,
             "refresh-token": refreshToken,
           },
-          body:({
+          body: JSON.stringify({
             ApiUrls,
           }),
         }
       );
-
+      const data = await response.json();
       if (response.ok) {
         // Show SweetAlert on success
         Swal.fire({
@@ -68,7 +78,9 @@ function Setting() {
         Swal.fire({
           icon: "error",
           title: "Fail",
-          text: "Setting update failed, please try again later",
+          text: data.error_Message
+            ? data.error_Message[0].message
+            : data.message,
           timer: 4500,
         });
       }
@@ -76,81 +88,55 @@ function Setting() {
       console.error("Update failed", error);
     }
   };
-  console.log(Setting);
-  console.log(ApiUrls);
-  // get all semetser
-  const fetchData = async () => {
-    try {
-      const response = await fetch(
-        `https://university-mohamed.vercel.app${routes.semster._id}${routes.semster.searchsemster}?page=1&size=20`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-            "refresh-token": refreshToken,
-          },
-        }
-      );
 
-      const data = await response.json();
-      setAllSemesters(data.semsters);
-    } catch (error) {
-      console.error("Fetch failed", error);
-    }
-  };
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `https://university-mohamed.vercel.app${routes.semster._id}${routes.semster.searchsemster}?page=1&size=20`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+              "refresh-token": refreshToken,
+            },
+          }
+        );
+
+        const data = await response.json();
+        setAllSemesters(data.semsters);
+      } catch (error) {
+        console.error("Fetch failed", error);
+      }
+    };
     fetchData();
   }, [accessToken, refreshToken]);
 
-  // change Main Semester
-  const ChangeMainSemester = async () => {
-    try {
-      const response = await fetch(
-        "https://university-mohamed.vercel.app/Api/admin/setting/update",
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`,
-            "refresh-token": refreshToken,
-          },
-          body: JSON.stringify({
-            MainSemsterId,
-            MaxAllowTrainingToRegister,
-          }),
-        }
-      );
-
-      if (response.ok) {
-        // Show SweetAlert on success
-        Swal.fire({
-          icon: "success",
-          title: "Setting updated successfully",
-          showConfirmButton: false,
-          timer: 3500,
-        });
-      } else {
-        // Show an error message if needed
-        Swal.fire({
-          icon: "error",
-          title: "Fail",
-          text: "Setting update failed, please try again later",
-          timer: 4500,
-        });
-      }
-    } catch (error) {
-      console.error("Update failed", error);
+  const handleAllowToggleChange = (url, checked) => {
+    const existingApiUrlIndex = ApiUrls.findIndex(
+      (apiUrl) => apiUrl.url === url
+    );
+    if (existingApiUrlIndex !== -1) {
+      const updatedApiUrls = [...ApiUrls];
+      updatedApiUrls[existingApiUrlIndex] = {
+        ...updatedApiUrls[existingApiUrlIndex],
+        allow: checked ? "yes" : "no",
+      };
+      setApiUrls(updatedApiUrls);
+    } else {
+      setApiUrls((prevApiUrls) => [
+        ...prevApiUrls,
+        { url: url, allow: checked ? "yes" : "no" },
+      ]);
     }
   };
-  // handle checked
 
-  console.log(allow);
   return (
     <>
       <div className="setting-page">
         <div className="single-setting">
-          <Table striped bordered hover class="table" style={{}}>
-            <thead class="thead-dark">
+          <Table striped bordered hover className="table">
+            <thead className="thead-dark">
               <tr>
                 <th scope="col">#</th>
                 <th style={{ textAlign: "center" }} scope="col">
@@ -163,34 +149,40 @@ function Setting() {
               </tr>
             </thead>
             <tbody>
-              {Setting.map((setting, index) => {
-                return (
-                  <tr key={setting._id}>
-                    <th scope="row">{index + 1}</th>
-                    <td style={{ textAlign: "center" }}>{setting.name}</td>
-                    <td style={{ textAlign: "center" }}>{setting.desc}</td>
-                    <td>
-                      <div style={{}} class="form-check form-switch">
-                        <input
-                          style={{ width: "70px", height: "20px" }}
-                          className="form-check-input"
-                          type="checkbox"
-                          role="switch"
-                          id={`flexSwitchCheck`}
-                          onChange={(e) => {
-                            setallow(e.target.checked === true ? "yes" : "no");
-                            setApiUrls([{"url":setting.url,
-                            "allow":{allow}
-                          }]);
-                          }}
-                        />
-                      </div>
-                    </td>
-                  </tr>
-                );
-              })}
+              {Setting.map((setting, index) => (
+                <tr key={setting._id}>
+                  <th scope="row">{index + 1}</th>
+                  <td style={{ textAlign: "center" }}>{setting.name}</td>
+                  <td style={{ textAlign: "center" }}>{setting.desc}</td>
+                  <td>
+                    <div className="form-check form-switch">
+                      <div
+                        style={{
+                          width: "50px",
+                          height: "25px",
+                          backgroundColor:
+                            ApiUrls.find((apiUrl) => apiUrl.url === setting.url)
+                              ?.allow === "yes"
+                              ? "green"
+                              : "red",
+                          borderRadius: "12.5px",
+                          cursor: "pointer",
+                        }}
+                        onClick={() => {
+                          handleAllowToggleChange(
+                            setting.url,
+                            ApiUrls.find((apiUrl) => apiUrl.url === setting.url)
+                              ?.allow !== "yes"
+                          );
+                        }}
+                      ></div>
+                    </div>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </Table>
+
           <div
             style={{ display: "flex", gap: "10px" }}
             className="Change-Main-semester"
@@ -204,9 +196,11 @@ function Setting() {
               }}
             >
               <option selected>Select Main Semester</option>
-              {AllSemesters.map((Sem) => {
-                return <option value={Sem._id}>{Sem.name}</option>;
-              })}
+              {AllSemesters.map((Sem) => (
+                <option key={Sem._id} value={Sem._id}>
+                  {Sem.name}
+                </option>
+              ))}
             </select>
             <select
               class="form-select form-select-md mb-3"
@@ -226,16 +220,6 @@ function Setting() {
               type="button"
               class="btn btn-primary "
               onClick={() => {
-                ChangeMainSemester();
-              }}
-            >
-              Change Main Semester
-            </button>
-            <button
-              style={{ height: "37px" }}
-              type="button"
-              class="btn btn-primary "
-              onClick={() => {
                 UpdateSettings();
               }}
             >
@@ -247,4 +231,6 @@ function Setting() {
     </>
   );
 }
+
 export default Setting;
+
