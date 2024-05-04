53b57ab3645c6b2c60f73ac719c8bfb8a2fd3cc0
omar <omarmokdad2022@gmail.com>
2024-05-05 00:33:23 +0300

    omar

---
+++ src/ComponentForSuberAdmin/Student_Grade.js
@@ -0,0 +1,494 @@
+import Select from "react-select";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "../styles/studentgradesearchbysuberadmin.css";
+import Swal from "sweetalert2";
+import { routes } from "../routes";
+import { Table } from "react-bootstrap";
+
+function StudentGrades() {
+  const accessToken = localStorage.getItem("accesstoken");
+  const refreshToken = localStorage.getItem("refreshtoken");
+  const [courseId, setcourseId] = useState("");
+  const [studentId, setstudentId] = useState("");
+  const [allcourses, setallcourses] = useState([]);
+  const [allstudents, setallstudents] = useState([]);
+  const [FinalExam, setFinalExam] = useState("");
+  const [Oral, setOral] = useState("");
+  const [Midterm, setMidterm] = useState("");
+  const [Practical, setPractical] = useState("");
+  const [AllSemesters, setAllSemesters] = useState("");
+  const [semsterId, setsemsterId] = useState("");
+  const [resultsbysuberadmin, setresultsbysuberadmin] = useState([]);
+  const [selectedGradeId, setselectedGradeId] = useState("");
+  const [backToRegister, setbackToRegister] = useState("yes");
+
+  useEffect(() => {
+    const fetchsearchforcourse = async () => {
+      try {
+        const response = await axios.get(
+          `https://university-mohamed.vercel.app/Api/courses/search/for/courses/by/Admin?search=&sort=course_name&page=1&size=20&select=course_name`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+              "refresh-token": refreshToken,
+            },
+          }
+        );
+        console.log(response.data);
+        const data = response.data;
+        console.log(data);
+        setallcourses(data.courses);
+        console.log(allcourses);
+      } catch (error) {
+        console.error("Error fetching search results:", error);
+      }
+    };
+    fetchsearchforcourse();
+  }, [accessToken, refreshToken]);
+
+  // get all students
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `https://university-mohamed.vercel.app/Api/students/search/for/students?sort=_id&select=Student_Code,National_Id,Full_Name,Date_of_Birth,PhoneNumber&page=1&size=20`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "refresh-token": refreshToken,
+          },
+        }
+      );
+
+      console.log(response.data);
+      setallstudents(response.data.students);
+    } catch (error) {
+      console.error("Error fetching admin info:", error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, [accessToken, refreshToken]);
+  console.log(allstudents);
+  // upload grade
+  const Upload_grade = async () => {
+    try {
+      const response = await fetch(
+        `https://university-mohamed.vercel.app/Api/students/Grades/add/grade/by/admin`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+            "refresh-token": refreshToken,
+          },
+          body: JSON.stringify({
+            courseId,
+            studentId,
+            semsterId,
+            Midterm,
+            Oral,
+            Practical,
+            FinalExam,
+          }),
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+      // setmessage(data.message);
+      if (response.ok) {
+        // Show SweetAlert on success
+
+        Swal.fire({
+          icon: "success",
+          title: "Upload grade successfully",
+          showConfirmButton: false,
+          timer: 3500,
+        });
+      } else {
+        // Show an error message if needed
+        Swal.fire({
+          icon: "error",
+          title: "Fail",
+          text: "Upload grade failed please try again later",
+          timer: 4500,
+        });
+      }
+    } catch (error) {
+      console.error("Upload grade failed", error);
+    }
+  };
+  console.log(courseId, studentId, semsterId);
+  // get all semester
+  const fetchDataa = async () => {
+    try {
+      const response = await fetch(
+        `https://university-mohamed.vercel.app/Api/semsters/search/semster?page=1&size=20`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "refresh-token": refreshToken,
+          },
+        }
+      );
+      const data = await response.json();
+      setAllSemesters(data.semsters);
+    } catch (error) {
+      console.error("Fetch failed", error);
+    }
+  };
+  useEffect(() => {
+    fetchDataa();
+  }, [accessToken, refreshToken]);
+  console.log(AllSemesters);
+  // search result of grades for students
+  const fetchDataaa = async () => {
+    try {
+      const response = await axios.get(
+        `https://university-mohamed.vercel.app/Api/students/Grades/search/by/admin?courseId=${courseId}&semsterId=${semsterId}&studentId=${studentId}&size=8&page=1&select=studentId,courseId,Points,Grade,TotalGrate,Midterm,Practical,Oral,FinalExam,YearWorks&sort=studentId.Full_Name`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "refresh-token": refreshToken,
+          },
+        }
+      );
+      console.log(response.data);
+      setresultsbysuberadmin(response.data.grades);
+    } catch (error) {
+      console.error("Error fetching doctor info:", error);
+    }
+  };
+  useEffect(() => {
+    fetchDataaa();
+  }, [accessToken, refreshToken, courseId, semsterId, studentId]);
+
+  // update grade
+  const updateGrade = async () => {
+    try {
+      const response = await fetch(
+        `https://university-mohamed.vercel.app/Api/students/Grades/update/grade/by/admin?GradeId=${selectedGradeId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+            "refresh-token": refreshToken,
+          },
+          body: JSON.stringify({
+            // studentId,
+            // courseId,
+            Oral,
+            Practical,
+            Midterm,
+            FinalExam,
+          }),
+        }
+      );
+      const data = await response.json();
+      if (response.ok) {
+        // Show SweetAlert on success
+        Swal.fire({
+          icon: "success",
+          title: "Grade updated successfully",
+          showConfirmButton: false,
+          timer: 3500,
+        });
+
+        // Update the state with the modified course
+        setresultsbysuberadmin((prevGrades) =>
+          prevGrades.map((prevGrade) =>
+            prevGrade._id === selectedGradeId
+              ? {
+                  ...prevGrade,
+                  // studentId,
+                  // courseId,
+                  Midterm,
+                  Oral,
+                  Practical,
+                  FinalExam,
+                }
+              : prevGrade
+          )
+        );
+
+        // Clear the selected course and reset input fields
+        setselectedGradeId(null);
+        setstudentId("");
+        setcourseId("");
+        setOral("");
+        setMidterm("");
+        setPractical("");
+        setFinalExam("");
+      } else {
+        // Show an error message if needed
+        Swal.fire({
+          icon: "error",
+          title: "Fail",
+          text: data.error_Message[0].message,
+          timer: 4500,
+        });
+      }
+    } catch (error) {
+      console.error("Update failed", error);
+    }
+  };
+  console.log(selectedGradeId);
+  // delete grade
+  const deleteGrade = async (GradeId) => {
+    try {
+      const confirmed = await Swal.fire({
+        title: "Are you sure?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "#3085d6",
+        confirmButtonText: "Yes, delete it!",
+      });
+      if (confirmed.isConfirmed) {
+        const response = await fetch(
+          `https://university-mohamed.vercel.app/Api/students/Grades/delete/grade/by/admin?GradeId=${GradeId}`,
+          {
+            method: "DELETE",
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+              "refresh-token": refreshToken,
+            },
+            body: JSON.stringify({
+              backToRegister,
+            }),
+          }
+        );
+
+        if (response.ok) {
+          // On success, update the state to remove the deleted course
+          setresultsbysuberadmin((prevGrade) =>
+            prevGrade.filter((grade) => grade._id !== GradeId)
+          );
+          console.log(`Course with ID ${courseId} deleted successfully.`);
+        } else {
+          console.error(`Failed to delete course with ID ${GradeId}.`);
+        }
+      }
+    } catch (error) {
+      console.error("Delete failed", error);
+    }
+  };
+  return (
+    <>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "30px",
+          alignItems: "center",
+          marginTop: "20px",
+        }}
+      >
+        <Select
+          isMulti
+          name="colors"
+          options={allcourses.map((course) => {
+            return { value: course._id, label: course.course_name };
+          })}
+          onChange={(selectedOptions) => {
+            const selectedLabels = selectedOptions.map(
+              (option) => option.value
+            );
+            setcourseId(selectedLabels.join());
+          }}
+          className="Materials_select2"
+          classNamePrefix="select"
+          placeholder="Select Course ID"
+        />
+        <Select
+          styles={{ width: "350px" }}
+          isMulti
+          name="colors"
+          options={allstudents.map((student) => {
+            return { value: student._id, label: student.Full_Name };
+          })}
+          onChange={(selectedOptions) => {
+            const selectedLabels = selectedOptions.map(
+              (option) => option.value
+            );
+            setstudentId(selectedLabels.join());
+          }}
+          className="Materials_select2"
+          classNamePrefix="select"
+          placeholder="Select Student ID"
+        />
+        <Select
+          styles={{ width: "350px" }}
+          isMulti
+          name="colors"
+          options={
+            AllSemesters
+              ? AllSemesters.map((sem) => {
+                  return { value: sem._id, label: sem.name };
+                })
+              : null
+          }
+          onChange={(selectedOptions) => {
+            const selectedLabels = selectedOptions.map(
+              (option) => option.value
+            );
+            setsemsterId(selectedLabels.join());
+          }}
+          className="Materials_select2"
+          classNamePrefix="select"
+          placeholder="Select Student ID"
+        />
+        <input
+          style={{ width: "30%", height: "40px" }}
+          className="form-control form-control-sm"
+          type="number"
+          name="FinalExam"
+          value={FinalExam}
+          onChange={(e) => {
+            setFinalExam(e.target.value);
+          }}
+          placeholder="Final Exam "
+          aria-label=".form-control-sm example"
+        />
+
+        <input
+          style={{ width: "30%", height: "40px" }}
+          className="form-control form-control-sm"
+          type="number"
+          name="Oral"
+          value={Oral}
+          onChange={(e) => {
+            setOral(e.target.value);
+          }}
+          placeholder="Oral"
+          aria-label=".form-control-sm example"
+        />
+        <input
+          style={{ width: "30%", height: "40px" }}
+          className="form-control form-control-sm"
+          type="number"
+          name="Practical"
+          value={Practical}
+          onChange={(e) => {
+            setPractical(e.target.value);
+          }}
+          placeholder="Practical"
+          aria-label=".form-control-sm example"
+        />
+        <input
+          style={{ width: "30%", height: "40px" }}
+          className="form-control form-control-sm"
+          type="number"
+          name="Midterm"
+          value={Midterm}
+          onChange={(e) => {
+            setMidterm(e.target.value);
+          }}
+          placeholder="Midterm"
+          aria-label=".form-control-sm example"
+        />
+        <button
+          style={{ width: "305px" }}
+          type="button"
+          className="btn btn-primary mt-1"
+          onClick={() => {
+            Upload_grade();
+          }}
+        >
+          Upload Grade
+        </button>
+        <button
+          style={{ width: "305px" }}
+          type="button"
+          className="btn btn-primary mt-1"
+          onClick={() => {
+            updateGrade();
+          }}
+        >
+          Update Grade
+        </button>
+      </div>
+      <Table
+        style={{ marginTop: "20px", textAlign: "center" }}
+        striped
+        bordered
+        hover
+        size="md"
+        className="col-12 table3"
+      >
+        <thead>
+          <tr>
+            <th scope="col">#ID</th>
+            <th scope="col">Student Name</th>
+            <th scope="col">Course Name</th>
+            <th scope="col">YearWorks</th>
+            <th scope="col">Practical</th>
+            <th scope="col">FinalExam</th>
+            <th scope="col">TotalGrate</th>
+            <th scope="col">Operation</th>
+          </tr>
+        </thead>
+        <tbody>
+          {resultsbysuberadmin.map((results, index) => {
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{results.studentId.Full_Name}</td>
+                <td>{results.courseId.course_name}</td>
+                <td>{results.YearWorks}</td>
+                <td>{results.Practical}</td>
+                <td>{results.FinalExam}</td>
+                <td>{results.TotalGrate}</td>
+                <td>
+                  <button
+                    style={{
+                      width: "80px",
+                      height: "40px",
+                      marginLeft: ".6rem",
+                      backgroundColor: "#996ae4",
+                      color: "white",
+                    }}
+                    onClick={() => {
+                      setcourseId(results.courseId._id);
+                      setstudentId(results.studentId._id);
+                      setMidterm(results.Midterm);
+                      setOral(results.Oral);
+                      setPractical(results.Practical);
+                      setFinalExam(results.FinalExam);
+                      setselectedGradeId(results._id);
+                    }}
+                    type="button"
+                    className="btn "
+                  >
+                    Update
+                  </button>
+                  <button
+                    style={{
+                      width: "80px",
+                      height: "40px",
+                      marginLeft: ".6rem",
+                      backgroundColor: "red",
+                      color: "white",
+                    }}
+                    onClick={() => {
+                      deleteGrade(results._id);
+                    }}
+                    type="button"
+                    className="btn "
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </>
+  );
+}
+
+export default StudentGrades;
--- src/pages/MainPageForSuberAdmin.js
+++ src/pages/MainPageForSuberAdmin.js
@@ -1,42 +1,44 @@
 // import React, { useEffect, useState } from "react";
-import SideBar from '../ComponentForSuberAdmin/SideBar'
-import NavBar from '../ComponentForSuberAdmin/NavBar'
-import '../styles/MainPage.css'
-import Poster from '../ComponentForSuberAdmin/Poster'
-import { useRecoilState } from 'recoil'
-import { $Dashboard_Components } from '../Atoms'
-import CreateAdmin from '../ComponentForSuberAdmin/CreateAdmin'
-import AllAdmins from '../ComponentForSuberAdmin/AllAdmins'
-import Setting from '../ComponentForSuberAdmin/Setting'
+import SideBar from "../ComponentForSuberAdmin/SideBar";
+import NavBar from "../ComponentForSuberAdmin/NavBar";
+import "../styles/MainPage.css";
+import Poster from "../ComponentForSuberAdmin/Poster";
+import { useRecoilState } from "recoil";
+import { $Dashboard_Components } from "../Atoms";
+import CreateAdmin from "../ComponentForSuberAdmin/CreateAdmin";
+import AllAdmins from "../ComponentForSuberAdmin/AllAdmins";
+import Setting from "../ComponentForSuberAdmin/Setting";
+import StudentGrades from "../ComponentForSuberAdmin/Student_Grade";
 // import axios from "axios";
 
-function MainPageForSuberAdmin () {
+function MainPageForSuberAdmin() {
   const [selectedComponent, setSelectedComponent] = useRecoilState(
     $Dashboard_Components
-  )
+  );
   return (
     <>
-      <div className='mainpage'>
-        <div className='sidebar'>
+      <div className="mainpage">
+        <div className="sidebar">
           <SideBar />
         </div>
-        <div className='container-page'>
-          <div className='navbar'>
+        <div className="container-page">
+          <div className="navbar">
             <NavBar />
           </div>
-          <div className='poster'>
+          <div className="poster">
             {/* Use the studentinfo state to pass data to Poster component */}
             <Poster />
           </div>
           <div>
-            {selectedComponent === 'addAdmin' && <CreateAdmin />}
-            {selectedComponent === 'getalladmin' && <AllAdmins />}
-            {selectedComponent === 'setting' && <Setting />}
+            {selectedComponent === "addAdmin" && <CreateAdmin />}
+            {selectedComponent === "getalladmin" && <AllAdmins />}
+            {selectedComponent === "setting" && <Setting />}
+            {selectedComponent === "studentgrade" && <StudentGrades />}
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default MainPageForSuberAdmin
+export default MainPageForSuberAdmin;
