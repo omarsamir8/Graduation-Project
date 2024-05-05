c86f2070b381bbacdda37e7058345bb3b709468d
omar <omarmokdad2022@gmail.com>
2024-05-05 13:55:37 +0300

    omar

--- src/pages/studentInfo/StudentInfo.jsx
+++ src/pages/studentInfo/StudentInfo.jsx
@@ -11,8 +11,8 @@ import { Button, Table } from "react-bootstrap";
 export default function StudentInfo() {
   const { studentId } = useParams();
 
-  const { semesterinfo, setsemesterinfo } = useState([]);
-  const [studentInfo, setStudentInfo] = useState([]);
+  let { semesterinfo, setsemesterinfo } = useState([]);
+  let [studentData, setstudentData] = useState({});
   const [studentnewspaper, setstudentnewspaper] = useState([]);
   const [totalgpa, settotalgpa] = useState("");
   const [totalhour, settotalhour] = useState("");
@@ -27,7 +27,8 @@ export default function StudentInfo() {
     const fetchSemesterGrade = async () => {
       try {
         const response = await axios.get(
-          `https://university-mohamed.vercel.app${routes.studentGrades._id}${routes.studentGrades.NewspaperBystudent}`,
+          `https://university-mohamed.vercel.app/Api/students/Grades/Get/Newspaper/By/Admin?studentId=${studentId}
+          `,
           {
             headers: {
               Authorization: `Bearer ${accessToken}`,
@@ -40,10 +41,6 @@ export default function StudentInfo() {
         settotalgpa(response.data.totalGpaOverall);
         settotalhour(response.data.totalCreditHours);
         setlevel(response.data.level);
-        setStudentInfo(response.data.semesters[0].studentId);
-        setsemesterinfo(response.data.semesters[0].semsterId);
-
-        // Log the updated state
       } catch (error) {
         console.error("Error fetching student info:", error);
       }
@@ -62,57 +59,110 @@ export default function StudentInfo() {
             <img src={profile_info} className="profile_photo" />
           </div>
           <div className="infoo">
-            <h4>
-              {studentInfo.FullName ? studentInfo.FullName : "Omar Samir"}{" "}
-            </h4>
-            <h5 style={{ marginTop: "-5px" }}>Level : One</h5>
+            {studentnewspaper[0] && studentnewspaper[0].studentId && (
+              <h4>
+                {studentnewspaper[0].studentId.Full_Name
+                  ? studentnewspaper[0].studentId.Full_Name
+                  : "loading"}
+              </h4>
+            )}
+
+            <h5 style={{ marginTop: "-5px" }}>Level : {level}</h5>
           </div>
         </div>
         <div className="single-details">
           <div className="det">
             <div className="single-det">
               <i class="fa-solid fa-signature"></i>
-              <h5>
-                {studentInfo.FullName ? studentInfo.FullName : "Omar Samir"}{" "}
-              </h5>
+              {studentnewspaper[0] && studentnewspaper[0].studentId && (
+                <h5>
+                  {studentnewspaper[0].studentId.Full_Name
+                    ? studentnewspaper[0].studentId.Full_Name
+                    : "loading"}
+                </h5>
+              )}
             </div>
             <div className="single-det">
               <i class="fa-solid fa-code"></i>
-              <h5>266120200100849</h5>
+              {studentnewspaper[0] && studentnewspaper[0].studentId && (
+                <h5>
+                  {studentnewspaper[0].studentId.Student_Code
+                    ? studentnewspaper[0].studentId.Student_Code
+                    : "loading"}
+                </h5>
+              )}
             </div>
             <div className="single-det">
               <i class="fa-solid fa-id-card"></i>
-              <h5>30207271400258</h5>
+              {studentnewspaper[0] && studentnewspaper[0].studentId && (
+                <h5>
+                  {studentnewspaper[0].studentId.National_Id
+                    ? studentnewspaper[0].studentId.National_Id
+                    : "loading"}
+                </h5>
+              )}
             </div>
             <div className="single-det">
               <i class="fa-solid fa-phone"></i>
-              <h5>01558849371 </h5>
+              {studentnewspaper[0] && studentnewspaper[0].studentId && (
+                <h5>
+                  {studentnewspaper[0].studentId.PhoneNumber
+                    ? studentnewspaper[0].studentId.PhoneNumber
+                    : "loading"}
+                </h5>
+              )}
             </div>
             <div className="single-det">
               <i class="fa-solid fa-cake-candles"></i>
-              <h5>27/7/2002 </h5>
+              {studentnewspaper[0] && studentnewspaper[0].studentId && (
+                <h5>
+                  {new Date(
+                    studentnewspaper[0].studentId.Date_of_Birth
+                  ).toLocaleDateString()}
+                </h5>
+              )}
             </div>
           </div>
           <div className="det">
             <div className="single-det">
               <i class="fa-solid fa-layer-group"></i>
-              <h5>{level ? level : "One"}</h5>
+              <h5>
+                {studentnewspaper[0] && studentnewspaper[0].semsterId && (
+                  <h5>
+                    {studentnewspaper[0].semsterId.term
+                      ? studentnewspaper[0].semsterId.term
+                      : "loading"}
+                  </h5>
+                )}
+              </h5>
             </div>
             <div className="single-det">
               <i class="fa-solid fa-calendar-days"></i>
-              <h5>2024-2025</h5>
+              {studentnewspaper[0] && studentnewspaper[0].semsterId && (
+                <h5>
+                  {studentnewspaper[0].semsterId.year
+                    ? studentnewspaper[0].semsterId.year
+                    : "loading"}
+                </h5>
+              )}
             </div>
             <div className="single-det">
               <i class="fa-solid fa-hourglass-start"></i>
-              <h5>{totalhour ? totalhour : "12"}</h5>
+              <h5>{totalhour ? totalhour : "loading"}</h5>
             </div>
             <div className="single-det">
               <i class="fa-brands fa-google"></i>
-              <h5>{totalgpa ? totalgpa : "3.5"}</h5>
+              <h5>{totalgpa ? totalgpa : "loading"}</h5>
             </div>
             <div className="single-det">
               <i class="fa-solid fa-venus-mars"></i>
-              <h5>Male</h5>
+              {studentnewspaper[0] && studentnewspaper[0].studentId && (
+                <h5>
+                  {studentnewspaper[0].studentId.gender
+                    ? studentnewspaper[0].studentId.gender
+                    : "loading"}
+                </h5>
+              )}
             </div>
           </div>
           <div className="det">
